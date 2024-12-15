4a6f19216d70eb5999951131e2892921c5f9e693
rajeshpanta <rpanta@sfsu.edu>
2024-12-14 16:11:39 -0800

    Initial Commit

---
+++ Lok-Citizenship/Lok_CitizenshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lok_CitizenshipApp.swift
+//  Lok-Citizenship
+//
+//  Created by Rajesh Panta on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Lok_CitizenshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
